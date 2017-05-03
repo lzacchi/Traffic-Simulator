d7cc0a534e85e7cf552a56901485f92d78095b9f
Lucas Zacchi de Medeiros <lzacchi@users.noreply.github.com>
2017-05-03 20:54:18 -0300

    Updated semaphore.h
    (Almost) complete

--- semaphore.h
+++ semaphore.h
@@ -16,10 +16,43 @@ public:
 
     ~Semaphore(){};
 
+    /**
+    *@brief     Coordinates the traffic lights at the intersection
+    *
+    *           i_ represents the lane for which the lights are green
+    *           if i_ == 4, the lights are red for every lane.
+    *
+    */
+    void cycle() {
+        i_ = (i_ + 1 % 5);
+    }
+
+    /**
+    * @brief     Verifies which lane can advance
+    *
+    * @param    lane
+    */
+    bool check(Lane* lane) {
+        if (i_ == 4) {
+            return false;
+        } else {
+            return lane == incoming[i_];
+        }
+    }
+
+    // void schedule(Controller* controller) {
+    //
+    // }
+
+
+
 protected:
     structures::LinkedList<int> list{};
 
 private:
+    Lane* incoming[4];
+    Lane* outgoing[4];
+    int i_;
 };
 
 }  // namespace structures
